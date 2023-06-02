96b96823b2bba2d87a8e17400c6325e57dc84505
Szymon <98458655+ochnios@users.noreply.github.com>
2023-06-02 20:39:04 +0200

    Update git aliases.

--- Windows/profile.ps1
+++ Windows/profile.ps1
@@ -2,7 +2,7 @@
 function Get-Git { & git $args }
 function Get-GitStatus { & git status $args }
 function Get-GitBranch { & git branch $args }
-function Get-GitCommit { & git commit $args }
+function Get-GitCommit { & git commit -m $args }
 function Get-GitPush { & git push $args }
 function Get-GitPull { & git pull $args }
 function Get-GitCheckout { & git checkout $args }
@@ -10,21 +10,38 @@ function Get-GitAdd { & git add $args }
 function Get-GitLog { & git log --oneline $args }
 function Get-GitRestoreStaged { & git restore --staged $args }
 function Get-GitResetHard { & git reset --hard $args }
+function Get-GitMerge { & git merge $args }
+function Get-GitCount {
+    $Like = $args[0]
+    $Files = git ls-files | Where-Object { $_ -like "*$Like" }
+    $TotalCount = 0
+    $Tab = [char]9
+    
+    foreach ($file in $Files) {
+        $Count = (Get-Content $File).Count
+        Write-Output "$Count$Tab$File"
+        $TotalCount += $Count
+    }
+    
+    Write-Output "$TotalCount$Tab total"
+}
 
 function Set-Aliases {
     $Aliases = @{
         # git aliases
-        g   = "Get-Git"
-        gs  = "Get-GitStatus"
-        gb  = "Get-GitBranch"
-        gc  = "Get-GitCommit"
-        gph = "Get-GitPush"
-        gpl = "Get-GitPull"
-        gco = "Get-GitCheckout"
-        ga  = "Get-GitAdd"
-        gl  = "Get-GitLog"
-        grs = "Get-GitRestoreStaged"
+        g      = "Get-Git"
+        gs     = "Get-GitStatus"
+        gb     = "Get-GitBranch"
+        gc     = "Get-GitCommit"
+        gph    = "Get-GitPush"
+        gpl    = "Get-GitPull"
+        gco    = "Get-GitCheckout"
+        ga     = "Get-GitAdd"
+        gl     = "Get-GitLog"
+        grs    = "Get-GitRestoreStaged"
         grhard = "Get-GitResetHard"
+        gmrg   = "Get-GitMerge"
+        gcount = "Get-GitCount"
     }
 
     foreach ($Alias in $Aliases.Keys) {
